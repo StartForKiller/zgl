f01d727fc67fe1d423db6c516bba746144e6cc4a
joshua.software.dev <joshua.software.dev@gmail.com>
2024-01-11 02:27:22 -0800

    Update for zig master 0.12.0-dev.2059

--- build.zig
+++ build.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("zgl", .{ .source_file = .{ .path = "zgl.zig" } });
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    _ = b.addModule("zgl", .{
+        .root_source_file = .{ .path = "zgl.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 }
